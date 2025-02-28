ee3cca395dbeaceb4fc6593e571e93f6c2937c54
mytestbotmy <40073544-mytestbotmy@users.noreply.replit.com>
2025-02-28 02:01:24 +0000

    Adjust padding on mobile to improve Debtera reading display.
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/235d6073-409a-4ed8-a0e2-019ae8bff921/ea57ada8-21cf-4e95-ba18-080ef04dfe12.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import DreamJournal from "@/pages/dream-journal";
 
 function Router() {
   return (
-    <div className="min-h-screen pt-6 md:pt-10 px-4 md:px-6">
+    <div className="min-h-screen pt-4 md:pt-8 px-4 md:px-6">
       <Switch>
         <Route path="/" component={Home} />
         <Route path="/dreams" component={DreamJournal} />
